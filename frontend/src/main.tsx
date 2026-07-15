0b79f085a189a98bb8fecdb64e83d7b7527fe954
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-07-15 17:28:44 +0000

    feat(router): configure routing for events and subscribers

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
 import './index.css'
 import App from './App.tsx'
+import Events from './components/Events.tsx'
+import Subscribers from './components/Subscribers.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<App />} />
+          <Route path="events" element={<Events />} />
+          <Route path="subscribers" element={<Subscribers />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
